9f0701eb1f217e1bdf902a8525fda38e088b7e66
unknown <lucasmalucha@gmail.com>
2019-12-09 20:04:49 +0000

    rework

---
+++ _rework/core/utils.py
@@ -0,0 +1,9 @@
+import random
+import string
+
+ALPHANUMERIC_CHARS = string.ascii_lowercase + string.digits
+STRING_LENGTH = 6
+
+
+def generate_random_string(chars=ALPHANUMERIC_CHARS, length=STRING_LENGTH):
+    return "".join(random.choice(chars) for _ in range(length))
---
+++ _rework/core/views.py
@@ -0,0 +1,15 @@
+from django.views.generic.base import TemplateView
+
+class IndexTemplateView(TemplateView):
+    def get_template_names(self):
+        template_name = "index.html"
+        return template_name
+
+def error_404(request):
+    return render(request, '404.html')
+
+
+def error_500(request):
+    return render(request, '500.html')
+
+
