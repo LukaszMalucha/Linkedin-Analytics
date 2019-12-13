e298a04dddb39fcc9c1f3c7540163ed1d428d8e2
unknown <lucasmalucha@gmail.com>
2019-12-13 12:44:35 +0000

    rework

--- db_manager/views.py
+++ db_manager/views.py
@@ -4,12 +4,12 @@ from db_manager.utils import database_upload, sector_insights
 
 def db_upload(request):
     """ Uncomment if refreshing data needed"""
-    database_upload()
+    # database_upload()
     return render(request, 'index.html')
 
 def sector_upload(request):
     """ Uncomment if refreshing data needed"""
-    sector_insights('finance')
-    sector_insights('it')
-    sector_insights('education')
+    # sector_insights('finance')
+    # sector_insights('it')
+    # sector_insights('education')
     return render(request, 'index.html')
