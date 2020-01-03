ef9d6c8ff01360bcf8abfc373fac6078d32f9c8c
unknown <lucasmalucha@gmail.com>
2020-01-03 07:53:25 +0000

    deployment

--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
 from django.views.generic.base import TemplateView
-
+from django.conf import settings
 
 class IndexTemplateView(TemplateView):
     def get_template_names(self):
-        template_name = "index.html"
+        if settings.DEBUG:
+            template_name = "index-dev.html"
+        else:
+            template_name = "index.html"
         return template_name
